24228cf9b5d0fa6adf66e3b93a543a05a1222054
Vitas1968 <dor1968@mail.ru>
2019-07-14 14:18:06 +0700

    решение задачи через  switch case

--- src/Lesson1/Seasons.java
+++ src/Lesson1/Seasons.java
@@ -14,6 +14,49 @@ package Lesson1;
 для зимних – «Winter», для весенних – «Spring», для осенних – «Autumn».
 Если число не соответствует возможному значению месяца, то в этом случае следует вывести «Error».*/
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Seasons
 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int [] salary = new  int[3];
+        PrintWriter out = new PrintWriter(System.out);
+
+        final String summer = "Summer";
+        final String winter = "Winter";
+        final String spring = "Spring";
+        final String autumn = "Autumn";
+        String season;
+
+        int monthNum= in.nextInt();
+
+        switch (monthNum) {
+            case 12:
+            case 1:
+            case 2:
+                season = winter;
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = spring;
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season =summer;
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = autumn;
+                break;
+            default:
+                season = "Error";
+        }
+        out.println(season);
+        out.flush();
+    }
 }
