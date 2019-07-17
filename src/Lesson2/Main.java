7096bd36ca22ac6f9b490709478a610180fe0e19
Vitas1968 <dor1968@mail.ru>
2019-07-17 21:11:44 +0700

    добавлен метод add() вставки элемента в указаррую позицию

--- src/Lesson2/Main.java
+++ src/Lesson2/Main.java
@@ -6,13 +6,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-         int [] arr = new int[1_00];
+         int [] arr = new int[1_0];
 
         for (int i = 0; i <arr.length ; i++)
         {
            arr[i] =  (int) (Math.random()*1000);//random.nextInt()/100;
         }
+
+
         print(arr);
+
+        //тест вставки элемента
+        print(add(arr,5,555));
 
 
 
@@ -26,5 +31,28 @@ public class Main
             System.out.print(i+" "+arr[i]+ "  " );
             if (i%10==0 ) System.out.println();
         }
+        System.out.println();
+    }
+
+    // добавление элемена
+    static int [] add(int [] array, int index, int element) {
+        if (index > array.length || index < 0) {
+            System.out.println("ArrayIndexOutOfBoundsException : index is out of range (index < 0 || index > size())");
+            System.exit(0);
+        }
+        int[] temp = new int[array.length + 1]; 
+
+        // здесь у тебя неправильное условие итератора i<=index;, отсюда и нули в окончание массива
+
+        for (int i = 0; i < array.length; i++) {
+            if (i < index) { // если текущий элемент меньше индекса вставки
+                temp[i] = array[i];  // то просто копируем
+            } else { //иначе копируем с сдвигом
+                temp[i + 1] = array[i];
+            }
+        }
+        temp[index] = element;
+        array = temp;
+        return array;
     }
 }
