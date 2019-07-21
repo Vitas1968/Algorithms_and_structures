c19bcdaeac588613dba6eac36b03a4f68771388f
Vitas1968 <dor1968@mail.ru>
2019-07-21 13:19:13 +0700

    тест реверс слова

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -4,6 +4,15 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        // реверс слова
+        String word ="12345678";
+        System.out.println("Исходное слово -> "+ word);
+        WordRevers wordRevers=new WordRevers(word);
+
+        System.out.println("Реверс слова -> "+wordRevers.reversWord());
+
+
     }
+
+
 }
