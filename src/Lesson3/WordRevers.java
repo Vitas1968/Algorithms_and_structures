856cd0e23075afbbb6b516cd87755730e6bb706e
Vitas1968 <dor1968@mail.ru>
2019-07-21 13:18:51 +0700

    реализация класса

--- src/Lesson3/WordRevers.java
+++ src/Lesson3/WordRevers.java
@@ -2,4 +2,30 @@ package Lesson3;
 
 public class WordRevers
 {
+    String word;
+    MyStack<Character> myStack;
+
+    public WordRevers(String word)
+    {
+        this.word = word;
+         this.myStack = new MyStack <>();
+    }
+
+    String reversWord()
+    { String tmp="";
+
+        for (int i = 0; i <word.length() ; i++)
+        {
+            myStack.push(word.charAt(i));
+           // System.out.print(word.charAt(i));
+        }
+        while (myStack.size()>0)
+        {
+            //System.out.print(myStack.pop());
+            tmp+= myStack.pop();
+        }
+
+
+        return tmp;
+    }
 }
