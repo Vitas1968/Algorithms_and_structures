9f5d818a14e5d5c417864d349e69d9cf3aefb8c6
Vitas1968 <dor1968@mail.ru>
2019-07-24 20:18:16 +0700

    Определены поля класса, констуктор без параметров и создан внутренний класс Node

--- src/Lesson4/MyLinkedList.java
+++ src/Lesson4/MyLinkedList.java
@@ -1,5 +1,41 @@
 package Lesson4;
 
-public class MyLinkedList
+public class MyLinkedList <Item>
 {
+    private Node first;
+    private int size = 0;
+
+    public MyLinkedList() {
+        this.first = null;
+    }
+
+    class Node<Item> {
+        private Item value;
+        private Node next;
+
+        public Node(Item value) {
+            this.value = value;
+        }
+
+        public Item getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setValue(Item value) {
+            this.value = value;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+    }
 }
