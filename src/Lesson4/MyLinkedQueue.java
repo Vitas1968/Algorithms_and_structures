e7182df71766363dccfbe89987dc84eaa9736556
Vitas1968 <dor1968@mail.ru>
2019-07-25 23:23:40 +0700

    добавлена реализация

--- src/Lesson4/MyLinkedQueue.java
+++ src/Lesson4/MyLinkedQueue.java
@@ -1,5 +1,26 @@
 package Lesson4;
 
-public class MyLinkedQueue
+public class MyLinkedQueue<T>
 {
+    private MyDoubleLinkedList<T> queue = new MyDoubleLinkedList<>();
+
+    public void enqueue(T value){
+        queue.insertFirst(value);
+    }
+
+    public T dequeue(){
+        return queue.removeLast();
+    }
+
+    public T peek(){
+        return queue.getLast();
+    }
+
+    public int size(){
+        return queue.size();
+    }
+
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
 }
