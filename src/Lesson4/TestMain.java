3f4b0bead6641dc3934d6574edaa2d3a5f602131
Vitas1968 <dor1968@mail.ru>
2019-07-25 19:22:11 +0700

    реализация тестирования всех методов класса MyLinkedList

--- src/Lesson4/TestMain.java
+++ src/Lesson4/TestMain.java
@@ -2,4 +2,69 @@ package Lesson4;
 
 public class TestMain
 {
+    public static void main(String[] args)
+    {
+        MyLinkedList <String> myLinkedList = new MyLinkedList <>();
+
+        // тест метода insertFirst
+        myLinkedList.insertFirst("Masha");
+        myLinkedList.insertFirst("Fedia");
+        myLinkedList.insertFirst("Vasia");
+        myLinkedList.insertFirst("Elen");
+        myLinkedList.insertFirst("Dog");
+
+        System.out.println("Содержание списка:");
+        System.out.println(myLinkedList);
+        drawLine();
+
+
+       // тест метода isEmpty()
+        System.out.println("Коллекция пустая? -> "+myLinkedList.isEmpty());
+        drawLine();
+
+        // тест метода size()
+        System.out.println("Размер коллекции -> "+ myLinkedList.size());
+        drawLine();
+
+        //тест метода getFirst()
+        System.out.println( "Значение первого элемента -> "+myLinkedList.getFirst());
+        drawLine();
+
+        //тест метода insert()
+        System.out.println("Содержание списка до вставки:");
+        System.out.println(myLinkedList);
+        myLinkedList.insert(3,"Cat");
+        System.out.println("Содержание списка после вставки:");
+        System.out.println(myLinkedList);
+        drawLine();
+
+        //тест метода contains
+
+        System.out.println("Список содежит "+ myLinkedList.contains("Cat"));
+        drawLine();
+
+        //тест метода removeFirst()
+        System.out.println("Содержание списка до вставки:");
+        System.out.println(myLinkedList);
+        System.out.println("Удален - > "+myLinkedList.removeFirst());
+        System.out.println("Содержание списка после удаления:");
+        System.out.println(myLinkedList);
+        drawLine();
+
+        // тест метода remove()
+        System.out.println("Содержание списка до удаления:");
+        System.out.println(myLinkedList);
+        System.out.println("Удален удачно- > "+myLinkedList.remove("Vasia"));
+        System.out.println("Содержание списка после удаления:");
+        System.out.println(myLinkedList);
+        drawLine();
+
+
+
+    }
+
+    static void drawLine()
+    {
+        System.out.println("----------------------------------------------------");
+    }
 }
