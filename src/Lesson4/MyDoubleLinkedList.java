e2c1ac91bf64848a44681a2724583dedadd96dd9
Vitas1968 <dor1968@mail.ru>
2019-07-25 19:44:57 +0700

    определены переменные класса, конструктор, класс итератора, класс Node

--- src/Lesson4/MyDoubleLinkedList.java
+++ src/Lesson4/MyDoubleLinkedList.java
@@ -1,5 +1,78 @@
 package Lesson4;
 
-public class MyDoubleLinkedList
+import java.util.Iterator;
+
+public class MyDoubleLinkedList <Item> implements Iterable<Item>
 {
+    private Node first;
+    private Node last;
+    private int size = 0;
+
+    public MyDoubleLinkedList() {
+        this.first = null;
+        this.last = null;
+    }
+    @Override
+    public Iterator<Item> iterator() {
+        return new Iter();
+    }
+
+    private class Iter implements Iterator<Item>{
+        Node current = new Node(null,first);
+
+        @Override
+        public boolean hasNext() {
+            return current.getNext() != null;
+        }
+
+        @Override
+        public Item next() {
+            current = current.getNext();
+            return (Item) current.getValue();
+        }
+    }
+
+    private class Node<Item> {
+        private Item value;
+        private Node next;
+        private Node previous;
+
+        public Node(Item value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(Item value) {
+            this.value = value;
+        }
+
+        public Item getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setValue(Item value) {
+            this.value = value;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+    }
 }
