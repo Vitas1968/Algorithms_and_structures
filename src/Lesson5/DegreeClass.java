9ce476a1e64eb7fa85afe16495894710a6630b62
Vitas1968 <dor1968@mail.ru>
2019-07-27 09:48:06 +0700

    реализация возведения в степень через рекурсию

--- src/Lesson5/DegreeClass.java
+++ src/Lesson5/DegreeClass.java
@@ -1,5 +1,20 @@
 package Lesson5;
 
-public class DegreeClfss
+public class DegreeClass
 {
+    public static void main(String[] args)
+    {
+        int m=2;
+        int n=4;
+
+        System.out.println("Число "+m+ " в степени "+n+" -> "+ degree(m,n));
+
+    }
+
+    static int degree(int m, int n)
+    {
+        if(n==1) return m;
+        else return m*degree(m,n-1);
+
+    }
 }
