41d54e85578703845ee17a4ed834236d398733b8
Vitas1968 <dor1968@mail.ru>
2019-07-28 17:22:19 +0700

    поля,конструктор,геттеры

--- src/Lesson5/Box.java
+++ src/Lesson5/Box.java
@@ -2,4 +2,22 @@ package Lesson5;
 
 public class Box
 {
+   protected Box box=null;
+   protected boolean key = false;
+
+    public Box(Box box, boolean key)
+    {
+        this.box = box;
+        this.key = key;
+    }
+
+    public Box getBox()
+    {
+        return box;
+    }
+
+    public boolean isKey()
+    {
+        return key;
+    }
 }
