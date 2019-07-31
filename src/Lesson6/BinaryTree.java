4c932435650549c69aed1d4b2063066d8499c356
Vitas1968 <dor1968@mail.ru>
2019-07-31 17:37:53 +0700

    создан внутренний класс Node

--- src/Lesson6/BinaryTree.java
+++ src/Lesson6/BinaryTree.java
@@ -1,5 +1,20 @@
 package Lesson6;
 
-public class BinaryTree
+public class BinaryTree <Key extends Comparable<Key>, Value>
 {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value value;
+        private Node left;
+        private Node right;
+        private int size;
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+            size = 1;
+        }
+    }
 }
