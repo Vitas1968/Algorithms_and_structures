7bab2db24f055202e003391e18258dabc9b9dd16
Vitas1968 <dor1968@mail.ru>
2019-07-31 18:29:01 +0700

    получить элемент по ключу

--- src/Lesson6/BinaryTree.java
+++ src/Lesson6/BinaryTree.java
@@ -4,14 +4,16 @@ public class BinaryTree <Key extends Comparable<Key>, Value>
 {
     private Node root;
 
-    private class Node {
+    private class Node
+    {
         private Key key;
         private Value value;
         private Node left;
         private Node right;
         private int size;
 
-        public Node(Key key, Value value) {
+        public Node(Key key, Value value)
+        {
             this.key = key;
             this.value = value;
             size = 1;
@@ -19,33 +21,69 @@ public class BinaryTree <Key extends Comparable<Key>, Value>
     }
 
     // размер коллекции
-    public int size() {
+    public int size()
+    {
         return size(root);
     }
 
-    private int size(Node node) {
-        if (node == null) {
+    private int size(Node node)
+    {
+        if (node == null)
+        {
             return 0;
         }
         return node.size;
     }
 
     // проверка наличия элементов в коллекции
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return root == null;
     }
 
     // проверка что ключ не null
-    private boolean isKeyNotNull(Key key) {
-        if (key == null) {
+    private boolean isKeyNotNull(Key key)
+    {
+        if (key == null)
+        {
             throw new IllegalArgumentException("Ключ не может быть null");
         }
         return true;
     }
 
     // содержится ли элемент в коллекции
-    public boolean contains(Key key) {
+    public boolean contains(Key key)
+    {
         isKeyNotNull(key);
         return get(key) != null;
     }
+
+
+    // получить элемент по ключу
+    public Value get(Key key)
+    {
+        return get(root, key);
+    }
+
+    private Value get(Node node, Key key)
+    {
+        isKeyNotNull(key);
+        if (node == null)
+        {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0)
+        {
+            return node.value;
+        } else if (cmp < 0)
+        {
+            return get(node.left, key);
+        } else
+        {
+            return get(node.right, key);
+        }
+    }
+
+
 }
