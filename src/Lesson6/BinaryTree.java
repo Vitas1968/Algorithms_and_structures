a6cba2bb71e01da62c3de2f82fd1157b2f709f05
Vitas1968 <dor1968@mail.ru>
2019-07-31 17:45:09 +0700

    проверка что ключ не null

--- src/Lesson6/BinaryTree.java
+++ src/Lesson6/BinaryTree.java
@@ -18,7 +18,7 @@ public class BinaryTree <Key extends Comparable<Key>, Value>
         }
     }
 
-    // размер
+    // размер коллекции
     public int size() {
         return size(root);
     }
@@ -33,5 +33,13 @@ public class BinaryTree <Key extends Comparable<Key>, Value>
     // проверка наличия элементов в коллекции
     public boolean isEmpty() {
         return root == null;
+    }
+
+    // проверка что ключ не null
+    private boolean isKeyNotNull(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Ключ не может быть null");
+        }
+        return true;
     }
 }
