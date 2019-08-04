b46123c69db0b55b61e0e96ea34228a4cf97f1d6
Vitas1968 <dor1968@mail.ru>
2019-08-04 19:43:21 +0700

    добавлен метод поиска в ширину

--- src/Lesson7/BreadthFirstPaths.java
+++ src/Lesson7/BreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package Lesson7;
 
+import java.util.LinkedList;
+
 public class BreadthFirstPaths
 {
     //массив посещенности
@@ -12,4 +14,36 @@ public class BreadthFirstPaths
     private final int INFINITY = Integer.MAX_VALUE;
     // исходная вершина с которой начинаем поиск
     private int source;
+
+    // конструктор
+    public BreadthFirstPaths(Graph g, int source) {
+        this.source = source;
+        edgeTo = new int[g.getVertexCount()];
+        marked = new boolean[g.getVertexCount()];
+        distTo = new int[g.getVertexCount()];
+        for (int i = 0; i < distTo.length; i++) {
+            distTo[i] = INFINITY;
+        }
+        bfs(g,source);
+    }
+
+    // метод поиска в ширину
+    private void bfs(Graph g, int source) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.addLast(source);
+        marked[source] = true;
+        distTo[source] = 0;
+
+        while (!queue.isEmpty()){
+            int vertex = queue.removeFirst();
+            for (int w:g.getAdjList(vertex)) {
+                if(!marked[w]){
+                    marked[w]= true;
+                    edgeTo[w] = vertex;
+                    distTo[w] = distTo[vertex] +1;
+                    queue.addLast(w);
+                }
+            }
+        }
+    }
 }
