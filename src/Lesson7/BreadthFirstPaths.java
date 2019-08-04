2207f8fc416e6ca53a2b4a3a7e0ca8989de091b4
Vitas1968 <dor1968@mail.ru>
2019-08-04 19:33:06 +0700

    определили переменные

--- src/Lesson7/BreadthFirstPaths.java
+++ src/Lesson7/BreadthFirstPaths.java
@@ -2,4 +2,14 @@ package Lesson7;
 
 public class BreadthFirstPaths
 {
+    //массив посещенности
+    private boolean[] marked;
+    // массив огкуда пришел
+    private int[] edgeTo;
+    // расстояние
+    private int[] distTo;
+    // самое возможно большое расстояние
+    private final int INFINITY = Integer.MAX_VALUE;
+    // исходная вершина с которой начинаем поиск
+    private int source;
 }
