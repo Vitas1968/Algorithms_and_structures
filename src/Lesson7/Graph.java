5c96818af20834b119df0a62534e259f88ef9551
Vitas1968 <dor1968@mail.ru>
2019-08-04 16:51:21 +0700

    описание переменных, конструктор

--- src/Lesson7/Graph.java
+++ src/Lesson7/Graph.java
@@ -1,5 +1,24 @@
 package Lesson7;
 
+import java.util.LinkedList;
+
 public class Graph
 {
+    //количество вершин
+    private int vertexCount;
+    //количество ребер
+    private int edgeCount=0;
+    //список смежностей
+    private LinkedList<Integer>[] adjList;
+    // конструктор
+    public Graph(int vertexCount) {
+        if(vertexCount<=0){
+            throw new IllegalArgumentException("Количество вершин не может быть < 0");
+        }
+        this.vertexCount = vertexCount;
+        adjList = new LinkedList[vertexCount];
+        for (int i = 0; i <vertexCount ; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+    }
 }
