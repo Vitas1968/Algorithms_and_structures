5c2fed30ade52533a0ad00a802deec419ff698b0
Vitas1968 <dor1968@mail.ru>
2019-08-04 18:30:53 +0700

    определение переменных

--- src/Lesson7/DepthFirstPaths.java
+++ src/Lesson7/DepthFirstPaths.java
@@ -2,4 +2,10 @@ package Lesson7;
 
 public class DepthFirstPaths
 {
+    //массив посещенности
+    private boolean[] marked;
+    // массив огкуда пришел
+    private int[] edgeTo;
+    // исходная вершина с которой начинаем поиск
+    private int source;
 }
