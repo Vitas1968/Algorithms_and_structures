de29c8b3644436010b05b54181ef4f7956c0b6d7
Vitas1968 <dor1968@mail.ru>
2019-08-07 22:55:09 +0700

    реализация цепочек

--- src/Lesson8/MainChain.java
+++ src/Lesson8/MainChain.java
@@ -2,4 +2,17 @@ package Lesson8;
 
 public class MainChain
 {
+    public static void main(String[] args)
+    {
+        ChainingHashMap <Integer, String> hm = new ChainingHashMap <>();
+        for (int i = 0; i <50 ; i++) {
+            hm.put((int)(Math.random()*1000),""+i);
+        }
+        System.out.println(hm);
+        System.out.println(hm.remove((int)(Math.random()*10))+"\n");
+        System.out.println(hm);
+
+
+
+    }
 }
