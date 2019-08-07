5ed81b369c597ed9cf2d3249d2d3e4f8f2c2d282
Vitas1968 <dor1968@mail.ru>
2019-08-07 19:29:08 +0700

    определены переменные внутренний класс узла и его конструктор

--- src/Lesson8/ChainingHashMap.java
+++ src/Lesson8/ChainingHashMap.java
@@ -1,5 +1,25 @@
 package Lesson8;
 
-public class ChainingHashMap
+import java.util.LinkedList;
+
+public class ChainingHashMap<Key, Value>
 {
+    // размер массива
+    private int capacity = 7;
+    // количество элементов в массиве
+    private int size = 0;
+    //сам массив типа связный список
+    private LinkedList<Node>[] st;
+
+    // узел, который везде в мапах называется Entry как я понял
+    class Node {
+        Key key;
+        Value value;
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+
+    }
 }
